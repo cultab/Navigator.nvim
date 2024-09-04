20bd5760c654e904cea888aaf6c069eebdbe7f9f
cultab <rroarck@gmail.com>
2024-09-04 23:25:35 +0300

    fix: improve windows detection
    See original fix: https://github.com/cultab/command.nvim/commit/a8a70c262be949d0506adbe6af5255568482f0ba

--- lua/Navigator/utils.lua
+++ lua/Navigator/utils.lua
@@ -4,20 +4,27 @@ local U = {}
 ---@param cmd string
 ---@return unknown
 function U.execute(cmd)
-    local handle = assert(io.popen(cmd .. " 2>&1"), string.format('[Navigator] Unable to execute cmd - %q', cmd))
+    local handle = assert(io.popen(cmd .. ' 2>&1'), string.format('[Navigator] Unable to execute cmd - %q', cmd))
     local result = handle:read()
     handle:close()
     return result
 end
 
----Returns executable suffix based on platform
----@return string
-function U.suffix()
-    local uname = vim.loop.os_uname()
-    if string.find(uname.release, 'WSL.*$') or string.find(uname.sysname, '^Win') then
-        return '.exe'
+local suffix_cache = nil
+--Returns executable suffix based on platform
+--- REF: Navigator.nvim
+--@return string
+U.suffix = function()
+    if not suffix_cache then
+        suffix_cache = '' -- default to empty, overwrite if it's windows
+        local uname = vim.loop.os_uname()
+        if string.find(uname.release, 'WSL.*$') or string.find(uname.sysname, '^Win') then -- may be windows
+            if os.execute('wezterm.exe') == 0 then -- if exe exists, execute if expensive so heuristics first
+                suffix_cache = '.exe'
+            end
+        end
+        return suffix_cache
     end
-    return ''
 end
 
 return U
