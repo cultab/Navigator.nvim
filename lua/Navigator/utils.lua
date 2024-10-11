2d883e073b88c60f42b6dda3fa9253445b538aec
cultab <rroarck@gmail.com>
2024-10-11 03:28:39 +0300

    fix: correctly hide stderr
    also pull suffix() from command.nvim

--- lua/Navigator/utils.lua
+++ lua/Navigator/utils.lua
@@ -4,27 +4,24 @@ local U = {}
 ---@param cmd string
 ---@return unknown
 function U.execute(cmd)
-    local handle = assert(io.popen(cmd .. ' 2>&1'), string.format('[Navigator] Unable to execute cmd - %q', cmd))
+    local handle = assert(io.popen(cmd .. '>/dev/null 2>&1'), string.format('[Navigator] Unable to execute cmd - %q', cmd))
     local result = handle:read()
     handle:close()
     return result
 end
 
 local suffix_cache = nil
---Returns executable suffix based on platform
---- REF: Navigator.nvim
---@return string
+---Returns executable suffix based on platform
+---@return string
 U.suffix = function()
-    if not suffix_cache then
-        suffix_cache = '' -- default to empty, overwrite if it's windows
-        local uname = vim.loop.os_uname()
-        if string.find(uname.release, 'WSL.*$') or string.find(uname.sysname, '^Win') then -- may be windows
-            if os.execute('wezterm.exe') == 0 then -- if exe exists, execute if expensive so heuristics first
-                suffix_cache = '.exe'
-            end
-        end
-        return suffix_cache
-    end
+	if not suffix_cache then
+		suffix_cache = '' -- default to empty, overwrite if it's windows
+		local obj = vim.system({ 'wezterm.exe', '--help' }):wait() -- if exe exists
+		if obj.code == 0 then
+			suffix_cache = '.exe'
+		end
+	end
+	return suffix_cache
 end
 
 return U
